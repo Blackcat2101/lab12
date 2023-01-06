b35bc407626dadf0b447b63b73672ab38d9ab4b4
mart <santhod11111@gmail.com>
2023-01-06 16:37:03 +0700

    lab12_1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std ;
+int main (){
+    cout << "Press Enter 3 times to reveal your future.";
+    string grade ;
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int i = rand()%9;
+    string x;
+    if (i == 0) x = "A" ;
+    else if (i==1) x = "B+";
+    else if (i==2) x = "B";
+    else if (i==3) x = "C+";
+    else if (i==4) x = "C";
+    else if (i==5) x = "D+";
+    else if (i==6) x = "D";
+    else if (i==7) x = "F";
+    else x = "W";
+
+
+
+    cout << "You will get " << x << " in this 261102."; 
+}
